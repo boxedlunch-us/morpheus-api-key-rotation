a8d73f6e4221750939b0ab5730298c473fe3b770
Ricky Nelson <rnelson@morpheusdata.com>
2020-11-17 11:35:35 -0600

    refresh with existing token

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import sys
-from morph_api_tomcart import refresh_access_token, create_cypher, get_current_token
+from morph_api_tomcart import refresh_access_token, create_cypher
+# TODO: push API check
+# TODO: check key expiration duration
+# TODO: motd countdown
+#
+
 
 # command line arguments - required
 appliance_name = sys.argv[1]
 client_id = sys.argv[2]  # morph-api, morph-automation, morph-cli, morph-customer
-username = sys.argv[3]
-password = sys.argv[4]
+refresh_token = sys.argv[3]
 
 # get current bearer token using on username/password
-current = get_current_token(appliance_name, client_id, username, password)
-response = refresh_access_token(appliance_name, client_id, current['refresh_token'])
+response = refresh_access_token(appliance_name, client_id, refresh_token)
 
 new_bearer = response['access_token']
 new_refresh = response['refresh_token']
 
 # create/update cypher entry for access/bearer token
 create_cypher(new_bearer, "access_token", appliance_name, new_bearer)
+create_cypher(new_refresh, "refresh_token", appliance_name, new_bearer)
