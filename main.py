d2e6608e037b18190082998277326b473e6a7e30
Ricky Nelson <rnelson@morpheusdata.com>
2020-12-03 10:34:40 -0600

    added handling for unlimited lease

--- main.py
+++ main.py
@@ -13,18 +13,17 @@ client_id = sys.argv[2]  # morph-api, morph-automation, morph-cli, morph-custome
 access_token = sys.argv[3]
 refresh_token = sys.argv[4]
 # get current bearer token using on username/password
-try:
-    expiry = days_until_expire(appliance_name, "access_token", access_token)
-    if expiry <= 1:
-        print("Rotating access token and updating API key")
-        response = refresh_access_token(appliance_name, client_id, refresh_token)
-        new_bearer = response['access_token']
-        new_refresh = response['refresh_token']
 
-        # create/update cypher entry for access/bearer token
-        create_cypher(new_bearer, "access_token", appliance_name, new_bearer, "3d")
-        create_cypher(new_refresh, "refresh_token", appliance_name, new_bearer, "3d")
-    else:
-        print("API key has not yet expired. " + str(expiry) + " days remaining on lease.")
-except:
-    print("Unable to update cypher entry")
+expiry = days_until_expire(appliance_name, "access_token", access_token)
+if expiry <= 1:
+    print("Rotating access token and updating API key")
+    response = refresh_access_token(appliance_name, client_id, refresh_token)
+    new_bearer = response['access_token']
+    new_refresh = response['refresh_token']
+
+    # create/update cypher entry for access/bearer token
+    create_cypher(new_bearer, "access_token", appliance_name, new_bearer, "3d")
+    create_cypher(new_refresh, "refresh_token", appliance_name, new_bearer, "3d")
+else:
+    print("API key has not yet expired. " + str(expiry) + " days remaining on lease.")
+
