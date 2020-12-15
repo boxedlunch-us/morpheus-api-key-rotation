86c86226d1075868c5d7cbed1b312d967e8a7a03
Ricky Nelson <rnelson@morpheusdata.com>
2020-12-15 15:27:08 -0600

    push api function start

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import sys
 from morph_api_tomcart import refresh_access_token, create_cypher, days_until_expire, execute_rest
 
-# asdf
-# TODO: push API check
-# TODO: check key expiration duration
-# TODO: motd countdown
-# 20201214
 # TODO: Failure conditions: lack of cypher entry, incorrect access token, incorrect refresh token
 #      1. Lack of API Access - DONE
 #      2. Cypher entry missing (access and/or refresh) - DONE
 #      3. Incorrect refresh token
+# TODO: push API check
 
-# 20201203
 # TODO: Setup Alert Rule - no response/bad response (job fail/connectivity)
 # TODO: Single notification on threshold meet
 # TODO: - deferred: additional/paralell check with differnt severitiy levelvvvlvlvlvl
+
 
 # command line arguments - required
 appliance_name = sys.argv[1]
 client_id = sys.argv[2]  # morph-api, morph-automation, morph-cli, morph-customer
 access_token = sys.argv[3]
 refresh_token = sys.argv[4]
+
+
+def push_api():
+    execute_rest("POST", push_api_url, access_token, "")
+
 
 # CRITICAL: Check to see if there is access to Morpheus API
 whoami_url = "https://" + appliance_name + "/api/whoami"
@@ -34,7 +35,7 @@ except KeyError:
 
     sys.exit("Unable to verify access to the API." + error_desc)
 
-# WARNING: Check for missing cypher entries - mainly refresh as we wouldn't make it this far without access token
+# CRITICAL: Check for missing cypher entries - mainly refresh as we wouldn't make it this far without access token
 secret_name = 'secret/refresh_token'
 cypher_url = "https://" + appliance_name + "/api/cypher/secret/" + secret_name
 try:
@@ -47,12 +48,12 @@ except ValueError:
 
     sys.exit("Unable to retrieve Cypher entry for " + secret_name)
 
-
 expiry = days_until_expire(appliance_name, "access_token", access_token)
-
 
 if expiry <= 1:
     print("Rotating access token and updating API key")
+
+    # New tokens to be passed on
     response = refresh_access_token(appliance_name, client_id, refresh_token)
     new_bearer = response['access_token']
     new_refresh = response['refresh_token']
