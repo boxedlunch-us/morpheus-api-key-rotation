84f79724009a0e2caeb493fab795d92926ea780e
Ricky Nelson <rnelson@morpheusdata.com>
2020-12-18 12:41:10 -0600

    config and debug files added for testing

--- debug.py
+++ debug.py
@@ -1,8 +1,8 @@
 import yaml
 
 with open("config.yaml", "r") as yaml_config:
-    cfg = yaml.load(yaml_config)
-print("")
+    cfg = yaml.load(yaml_config, Loader=yaml.FullLoader)
+print(cfg)
 # for section in cfg:
 #     print(section)
 # print(cfg["mysql"])
